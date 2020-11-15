4a9d2b2b1891ae36ca019e8289b2c076f6ec9558
RodGpe <rodrigo.ch1995@gmail.com>
2020-11-14 19:39:50 -0600

    "mover un circulo"

--- main.js
+++ main.js
@@ -8,4 +8,30 @@ window.onload = function(){
         y : height / 2,
         radius : 20
       };
+  draw();
+
+  function draw(){
+    context.clearRect(0,0,width,height);
+    context.fillStyle = "gray";
+    context.beginPath();
+    context.arc(handle.x, handle.y, handle.radius, 0 , Math.PI * 2, false);
+    context.fill();
+  }
+
+  document.body.addEventListener("mousedown",function(event){
+    if(utils.circlePointCollision(event.clientX,event.clientY,handle)){
+      document.body.addEventListener("mousemove", onMouseMove);
+      document.body.addEventListener("mouseup", onMouseUp);
+    }
+  });
+  function onMouseMove(event){
+    handle.x = event.clientX;
+    handle.y = event.clientY;
+    draw();
+  }
+
+  function onMouseUp (event){
+    document.body.removeEventListener("mousemove", onMouseMove);
+    document.body.removeEventListener("mouseup", onMouseUp);
+  }
 };
