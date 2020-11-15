69d7ae84b14472b79768a9dcaddd7d3871b8e1e8
RodGpe <rodrigo.ch1995@gmail.com>
2020-11-14 19:06:19 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  var canvas = document.getelementById("canvas"),
+      context = canvas.width = window.innerWidth,
+      width = canvas.widht = window.innerWidth,
+      height = canvas.height = window.innerHeight,
+      handle = {
+        x : width / 2,
+        y : height / 2,
+        radius : 20
+      };
+};
